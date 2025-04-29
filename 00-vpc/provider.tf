82b6e20dd51227a091b7cce28a742f6cc68f738e
Ambica <mukeshpuppala28@gmail.com>
2025-04-29 02:43:02 -0700

    tf

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
 backend "s3" {
-    bucket = "terraform-remotestate-file-dev" 
+    bucket = "terraform-remotestate-file" 
     key    = "expense-infra-dev-vpc" 
     region = "us-east-1"
     dynamodb_table = "state-locking"
