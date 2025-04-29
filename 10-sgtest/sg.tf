fc6561f7cfaf2312480795b27a782af296602e8b
Ambica <mukeshpuppala28@gmail.com>
2025-04-29 02:42:44 -0700

    tf

---
+++ 10-sgtest/sg.tf
@@ -0,0 +1,40 @@
+module "mysql_sg" {
+  source = "git::https://github.com/AmbicaPuppala/terraform-aws-securitygroup.git?ref=main"
+  project = var.project
+  environment = var.environment
+  sg_name = "mysql"
+  sg_description = "created for mysql instances in expense dev infra"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+}
+
+
+module "backend_sg" {
+  source = "git::https://github.com/AmbicaPuppala/terraform-aws-securitygroup.git?ref=main"
+  project = var.project
+  environment = var.environment
+  sg_name = "backend"
+  sg_description = "created for backend instances in expense dev infra"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+}
+
+module "frontend_sg" {
+  source = "git::https://github.com/AmbicaPuppala/terraform-aws-securitygroup.git?ref=main"
+  project = var.project
+  environment = var.environment
+  sg_name = "frontend"
+  sg_description = "created for frontend instances in expense dev infra"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+}
+
+module "bastion_sg" {
+  source = "git::https://github.com/AmbicaPuppala/terraform-aws-securitygroup.git?ref=main"
+  project = var.project
+  environment = var.environment
+  sg_name = "bastion"
+  sg_description = "created for bastion instances in expense dev infra"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+}
