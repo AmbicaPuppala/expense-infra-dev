547e30b05998bf3e9b45e1912dcc9db0c9004894
Ambica <mukeshpuppala28@gmail.com>
2025-04-30 03:38:24 -0700

    tf

--- 10-sgtest/sg.tf
+++ 10-sgtest/sg.tf
@@ -39,6 +39,17 @@ module "bastion_sg" {
   common_tags = var.common_tags
 }
 
+# ports 22, 443, 1194, 943 --> VPN ports
+module "vpn_sg" {
+  source = "git::https://github.com/AmbicaPuppala/terraform-aws-securitygroup.git?ref=main"
+  project = var.project
+  environment = var.environment
+  sg_name = "vpn"
+  sg_description = "created for vpn in expense dev infra"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+}
+
 module "app_alb_sg"{
   source = "git::https://github.com/AmbicaPuppala/terraform-aws-securitygroup.git?ref=main"
   project = var.project
@@ -63,7 +74,60 @@ resource "aws_security_group_rule" "bastion_public" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks       = ["49.37.152.212/32"]
+    cidr_blocks       = ["0.0.0.0/0"]
     security_group_id = module.bastion_sg.sg_id
     
+}
+
+
+resource "aws_security_group_rule" "vpn_ssh" {
+  type              = "ingress"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"] #it should be static ip of the user who is going to connect to the VPN
+    security_group_id = module.vpn_sg.sg_id
+    
+}
+
+
+resource "aws_security_group_rule" "vpn_443" {
+  type              = "ingress"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"] #it should be static ip of the user who is going to connect to the VPN
+    security_group_id = module.vpn_sg.sg_id
+    
+}
+
+
+resource "aws_security_group_rule" "vpn_943" {
+  type              = "ingress"
+    from_port        = 943
+    to_port          = 943
+    protocol         = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"] #it should be static ip of the user who is going to connect to the VPN
+    security_group_id = module.vpn_sg.sg_id
+    
+}
+
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type              = "ingress"
+    from_port        = 1194
+    to_port          = 1194
+    protocol         = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"] #it should be static ip of the user who is going to connect to the VPN
+    security_group_id = module.vpn_sg.sg_id
+    
+}
+
+resource "aws_security_group_rule" "app_alb_vpn" {
+  type              = "ingress"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    source_security_group_id = module.vpn_sg.sg_id
+    security_group_id = module.app_alb_sg.sg_id
 }
