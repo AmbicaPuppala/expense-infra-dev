509b63ae9d7acfa9a9d756283fc0972d98eaf603
Ambica <mukeshpuppala28@gmail.com>
2025-05-07 10:48:59 -0700

    tf

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -4,3 +4,4 @@ dnf install ansible -y
 #ansible-playbook -i inventory.ini mysql.yaml
 
 #pull architecture
+ansible-pull -i localhost -U https://github.com/AmbicaPuppala/expense-ansible-roles-tf.git -e COMPONENT=backend -e ENVIRONMENT=dev
--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -14,13 +14,22 @@ resource "aws_instance" "backend" {
 
 resource "null-resource" "expense" {
   
-  triggers = {
-    instance_id = aws_instance.backend.id
-  }
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
   connection {
     host        = aws_instance.backend.private_ip
     type        = "ssh"
     user       = "ec2-user"
     password  = "DevOps321"
   }
- 
+  provisioner "file" {
+    source      = "backend.sh"
+    destination = "/tmp/backend.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/backend.sh",
+      "sudo sh /tmp/backend.sh"
+    ]
+  }
+}
