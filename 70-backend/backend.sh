048cffcb5508f690ea4156b315caf2498abf73c5
Ambica <mukeshpuppala28@gmail.com>
2025-05-07 21:18:38 -0700

    tf

---
+++ 70-backend/backend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ENVIRONMENT=$1
+dnf install ansible -y
+#push architecture
+#ansible-playbook -i inventory.ini mysql.yaml
+
+#pull architecture
+ansible-pull -i localhost, -U https://github.com/AmbicaPuppala/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
