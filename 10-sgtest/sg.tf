841d6c599ca4ea1c7f82bc8107f8bc12a108353b
Ambica <mukeshpuppala28@gmail.com>
2025-05-13 00:34:32 -0700

    tf

--- 10-sgtest/sg.tf
+++ 10-sgtest/sg.tf
@@ -189,9 +189,9 @@ resource "aws_security_group_rule" "mysql_backend" {
 
 resource "aws_security_group_rule" "backend_app-alb" {
   type              = "ingress"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     source_security_group_id = module.app_alb_sg.sg_id
     security_group_id = module.backend_sg.sg_id
 }
